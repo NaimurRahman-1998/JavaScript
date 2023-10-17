8128bdf3dd16f5c3da44d2cbc8006d2a5a09edff
NaimurRahman-1998 <nrturzo1998111@gmail.com>
2023-10-17 19:07:52 +0600

    asd

--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 ### Arrays are Objects
 ### To solve this problem ECMAScript 5 (JavaScript 2009) defined a new method Array.isArray():
+
+### Sort
+# Array.sort() sorts string only and it is case sensetive
---
+++ sort.js
@@ -0,0 +1,10 @@
+const fruits = ["apple","banana","zebra","cele"]
+
+// console.log(fruits.sort())
+
+
+const number = [1842,12321456,43.2314213,234,123.324,213.1234]
+
+// console.log(number.sort(function(a,b){return a -b}))
+
+console.log(Math.random()*100)
