3eff1ca9b9ec986d6f8d93004733e19f863c3290
NaimurRahman-1998 <nrturzo1998111@gmail.com>
2023-10-17 12:37:11 +0600

    asd

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello")
+const cars = ['BMW', "VOLVO" , "VOLKS"];
+
+// document.getElementById("demo").innerHTML = cars
+
+let text = "<ul>";
+
+for (i = 0; i < cars.length ; i++){
+    text += `<li> ${cars[i]} </li>`
+}
+
+text += "</ul>"
+
+document.getElementById("demo").innerHTML = text
---
+++ readme.md
@@ -0,0 +1,2 @@
+### Arrays are Objects
+### To solve this problem ECMAScript 5 (JavaScript 2009) defined a new method Array.isArray():
